36822d615e5da6a00357ea1288735c7081d805e9
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-11-13 13:46:53 -0800

    Add file upload with form as FormData object.
    Also installed jQuery with Select2.

--- src/app/auscultation-component/auscultation-component.component.ts
+++ src/app/auscultation-component/auscultation-component.component.ts
@@ -1,5 +1,5 @@
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-auscultation-component',
@@ -8,20 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuscultationComponentComponent implements OnInit {
   form: FormGroup;
+  loading: boolean = false;
+  @ViewChild('fileInput') fileInput: ElementRef;
+
   diseases = [
-    { id: 1, text: 'Disease 1'},
-    { id: 1, text: 'Disease 2'},
-    { id: 1, text: 'Disease 3'},
-    { id: 1, text: 'Disease 4'},
-    { id: 1, text: 'Disease 5'}
+    { id: 1, text: 'Disease 1' },
+    { id: 1, text: 'Disease 2' },
+    { id: 1, text: 'Disease 3' },
+    { id: 1, text: 'Disease 4' },
+    { id: 1, text: 'Disease 5' }
   ];
 
   positions = [
-    { id: 1, text: 'Position 1'},
-    { id: 1, text: 'Position 2'},
-    { id: 1, text: 'Position 3'},
-    { id: 1, text: 'Position 4'},
-    { id: 1, text: 'Position 5'}
+    { id: 1, text: 'Position 1' },
+    { id: 1, text: 'Position 2' },
+    { id: 1, text: 'Position 3' },
+    { id: 1, text: 'Position 4' },
+    { id: 1, text: 'Position 5' }
   ];
 
   constructor(fb: FormBuilder) {
@@ -31,12 +34,39 @@ export class AuscultationComponentComponent implements OnInit {
       diseases: ['', Validators.required],
       position: ['', Validators.required],
       patientReport: ['', Validators.required],
-      recording: ['', Validators.required]
+      recordings: ['', Validators.required]
     });
   }
 
+  onFileChange(event) {
+    let reader = new FileReader();
+    let myFileInput: HTMLInputElement = event.target;
+    if (event.target.files && event.target.files.length > 0) {
+      let file = event.target.files[0];
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.form.get('recordings').setValue({
+          filename: file.name,
+          filetype: file.type,
+          value: reader.result.split(',')[1]
+        })
+      };
+    }
+  }
+
+  clearFile() {
+    this.form.get('recordings').setValue(null);
+    this.fileInput.nativeElement.value = '';
+  }
+
   formSubmit() {
-    
+    const formModel = this.form.value;
+    this.loading = true;
+    // this.http.post('apiUrl', formModel)
+    setTimeout(() => {
+      alert('done!');
+      this.loading = false;
+    }, 1000);
   }
 
   ngOnInit() {
